8c83c8cd813038f046a4fc477ab83171e5ced6cb
ArunoxPL <greg.malek@gmail.com>
2017-10-17 12:19:05 +0200

    Starting the Task

---
+++ CapgeminiTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapgeminiTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

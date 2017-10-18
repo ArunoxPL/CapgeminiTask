3262fe1a7baf0f2a702d76bf79ef211539063abc
ArunoxPL <greg.malek@gmail.com>
2017-10-18 16:50:14 +0200

    xml tyrout

--- CapgeminiTask/Customer.cs
+++ CapgeminiTask/Customer.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Xml.Linq;
+
 
 namespace CapgeminiTask
 {
-    public struct Customer : ICustomer
+    public class Customer
     {
-        private string Name { get; set; }
-        private string Surname { get; set; }
-        private int TelephoneNumber { get; set; }
-        private string Address { get; set; }
+        private readonly List<Customer> _customerList = new List<Customer>();
 
-        public void AddCustomer()
+        protected string Name { get; set; }
+        protected string Surname { get; set; }
+        protected int TelephoneNumber { get; set; }
+        protected string Address { get; set; }
+
+        public Customer(string name, string surname, int telephoneNumber, string address)
+        {
+            Name = name;
+            Surname = surname;
+            TelephoneNumber = telephoneNumber;
+            Address = address;
+        }
+
+        public void CreatingXml()
+        {
+            XDocument xml = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("Customer list"),
+                new XElement("customers",
+                    from customer in _customerList
+                    orderby customer.Name
+                    select new XElement("customer",
+                        new XAttribute("name", customer.Name),
+                        new XElement("surname", customer.Surname),
+                        new XElement("telephone", customer.TelephoneNumber),
+                        new XElement("address", customer.Address)
+                    )
+                )
+            );
+            Console.WriteLine(xml);
+            MemoryStream mStrm = new MemoryStream(Encoding.UTF8.GetBytes("customers.xml"));
+            xml.Save(mStrm);
+        }
+
+
+        public void AddingCustomer()
         {
             Console.WriteLine("Write the Name of customer: ");
             Name = Console.ReadLine();
@@ -21,9 +57,12 @@ namespace CapgeminiTask
             TelephoneNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Write the Address of customer: ");
             Address = Console.ReadLine();
+
+            _customerList.Add(new Customer(Name, Surname, TelephoneNumber, Address));
+
+            
+
         }
-
-
 
     }
 
--- CapgeminiTask/Worker.cs
+++ CapgeminiTask/Worker.cs
@@ -6,53 +6,62 @@ namespace CapgeminiTask
 {
     public class Worker
     {
-        Customer customer = new Customer();
-        //Menu
+        Customer customer = new Customer("", "", 0, "");
+        bool exit = false;
+        
         public Worker()
         {
-            Console.WriteLine("Welcome in Menu, what do you want to do? \n Add new Customer press 1 \n Update Customer press 2 \n Delete Customer press 3 \n Exit press 4");
         }
-
-        public void Menu()
-        {
-            int? result = null;
-            
-            try
+       
+            public void Menu()
             {
-                result = Convert.ToInt16(Console.ReadLine());
-               
-                switch (result)
+                do
                 {
-                    case 1:
+                    int? result = null;
+                Console.WriteLine("Welcome in Menu, what do you want to do? \n Add new Customer press 1 \n Update Customer press 2 \n Delete Customer press 3 \n Exit press 4");
+
+                try
                     {
-                        customer.AddCustomer();
-                        Console.WriteLine(" Add menu works");
+                        result = Convert.ToInt16(Console.ReadLine());
+
+                        switch (result)
+                        {
+                            case 1:
+                            {
+                                customer.AddingCustomer();
+                                Console.WriteLine(" Add menu works");
+                            }
+                                break;
+                            case 2:
+                            {
+                                Console.WriteLine(" Delete menu works");
+
+                            }
+                                break;
+                            case 3:
+                                Console.WriteLine(" Update menu works");
+                                break;
+                            case 4:
+                            {
+                                exit = true;
+                                Console.WriteLine(" Exit menu works");
+                            }
+                                break;
+                            default:
+                                Console.WriteLine(" Wrong Parameter Works");
+                                break;
+                        }
                     }
-                        break;
-                    case 2:
-                        Console.WriteLine(" Delete menu works");
-                        break;
-                    case 3:
-                        Console.WriteLine(" Update menu works");
-                        break;
-                    case 4:
-                        Console.WriteLine(" Exit menu works");
-                        break;
-                    default:
-                        Console.WriteLine(" Wrong Parameter Works");
-                        break;
-                }
-            }
-            catch (FormatException fEx)
-            {
-                Console.WriteLine(fEx.Message);
-            }
-            catch (Exception Ex)
-            {
-                Console.WriteLine("Something went wrong");
-            }
+                    catch (FormatException fEx)
+                    {
+                        Console.WriteLine(fEx.Message);
+                    }
+                    catch (Exception Ex)
+                    {
+                        Console.WriteLine("Something went wrong");
+                    }
 
-        }
-
+                } while (exit != true);
+            }
     }
 }
